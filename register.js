a7f091810e7e604c53b4b698cb62aef727ffbfb0
AlirezaSharifManataz <165467116+AlirezaSharifManataz@users.noreply.github.com>
2024-05-02 14:25:09 +0330

    add mysql2

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const router = express.Router();
 
 // MySQL connection configuration
