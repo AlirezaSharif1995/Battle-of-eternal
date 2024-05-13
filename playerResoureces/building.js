762771510d0d7833095b39868085b1d8eb417f56
AlirezaSharif1995 <144997350+AlirezaSharif1995@users.noreply.github.com>
2024-05-13 16:20:01 +0430

    Update building.js

--- playerResoureces/building.js
+++ playerResoureces/building.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 
     try {
         // Check if the user exists in the database
-        const [existingUser] = await pool.query('SELECT * FROM userbuildings WHERE userId = ?', [userId]);
+        const [existingUser] = await pool.query('SELECT * FROM userbuildings WHERE playerToken = ?', [userId]);
 
         if (existingUser.length === 0) {
             return res.status(404).json({ error: 'User not found' });
