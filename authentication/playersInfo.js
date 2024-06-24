343f3c2c9126fdc449af66f4b4916eee56259666
AlirezaSharif1995 <144997350+AlirezaSharif1995@users.noreply.github.com>
2024-06-24 16:49:16 +0430

    Update playersInfo.js

--- authentication/playersInfo.js
+++ authentication/playersInfo.js
@@ -91,5 +91,78 @@ router.post('/changeMedals', async (req, res) => {
     }
 });
 
+router.post('/playerBorder', async (req, res) => {
+    const { email, borderX, borderY } = req.body;
+
+    try {
+        const [existingUser] = await pool.query('SELECT * FROM playeremailtable WHERE email = ?', [email]);
+
+        if (existingUser.length > 0) {
+            return res.status(400).json({ error: 'Email is already registered' });
+        }
+
+        const ID = generateRandomToken();
+        await pool.query('INSERT INTO playeremailtable (id, email, borderX, BorderY) VALUES (?, ?, ?, ?)', [ID, email, borderX, borderY]);
+        res.status(201).json({ message: 'Email registerd succesfully!' });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+
+});
+
+router.post('/getPlayerBorder', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const [existingUser] = await pool.query('SELECT * FROM playeremailtable WHERE email = ?', [email]);
+
+        if (existingUser.length < 0) {
+            return res.status(400).json({ error: 'User not found!' });
+        }
+
+        const user = {
+            borderX: existingUser[0].borderX,
+            borderY: existingUser[0].borderY,
+            civilization: existingUser[0].civilization,
+            allPopulation: existingUser[0].allPopulation,
+            users: existingUser[0].users
+        } 
+        res.status(201).json({ user });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Internal server error' });
+    }
+
+});
+
+router.post('/updatePlayerBorder', async (req, res) => {
+    const { email, type, data } = req.body;
+
+    try {
+        const [existingUser] = await pool.query('SELECT * FROM playeremailtable WHERE email = ?', [email]);
+
+        if (existingUser.length < 0) {
+            return res.status(400).json({ error: 'User not found' });
+        }
+
+        const url = `UPDATE playeremailtable SET ${type} = ? WHERE email = ?`
+        await pool.query(url, [data, email]);
+        res.status(201).json({ message: 'updated succesfully!' });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+
+});
+
+function generateRandomToken() {
+    let token = '';
+    for (let i = 0; i < 5; i++) {
+        token += Math.floor(Math.random() * 10); // Generate random digit (0-9)
+    }
+    return token;
+}
 
 module.exports = router;
